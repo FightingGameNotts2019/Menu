9c16f73f09a4d3be10745292fbb46e4a1e9e2818
rudiban <58337744+rudiban@users.noreply.github.com>
2019-12-03 14:16:12 +0000

    Add files via upload

--- menu.py
+++ menu.py
@@ -6,12 +6,9 @@ Created on Wed Nov 27 18:17:35 2019
 """
 
 import pygame
-import time
-import random
-import main
 import gameScreen
 
- 
+
 def quitgame():
     pygame.quit()
     quit()
@@ -28,7 +25,8 @@ def button(msg,x,y,w,h,ic,ac,action=None):
         pygame.draw.rect(screen, ac,(x,y,w,h))
 
         if click[0] == 1 and action != None:
-            action()         
+            action()
+            
     else:
         pygame.draw.rect(screen, ic,(x,y,w,h))
 
@@ -37,11 +35,9 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     screen.blit(textSurf, textRect)
 
-def game_intro():
+def mainMenu():
 
-    intro = True
-
-    while intro:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -53,18 +49,65 @@ def game_intro():
         TextRect.center = ((display_width/2),(display_height*0.15)) 
         screen.blit(TextSurf, TextRect)
 
-        button("Fight!",800,200,100,50,green,bright_green,main.gameLoop)
-        button("Tutorial?",800,300,100,50,red,bright_red,quitgame)
-        button("Quit",800,400,100,50,red,bright_red,quitgame)
+        button("Fight!",500,200,100,50,green,bright_green,stageMenu )
+        button("Tutorial",500,300,100,50,green,bright_green,tutorialMenu)
+        button("Quit",500,400,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+        
+def stageMenu():
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill(white) 
+        largeText = pygame.font.SysFont("comicsansms",90) 
+        TextSurf, TextRect = text_objects("Level Select", largeText) 
+        TextRect.center = ((display_width/2),(display_height*0.15)) 
+        screen.blit(TextSurf, TextRect)
+
+        button("Stage 1",800,200,100,50,green,bright_green,quitgame)
+        button("Stage 2",800,300,100,50,green,bright_green,quitgame)
+        button("Stage 3",800,400,100,50,green,bright_green,quitgame)
+        button("Menu",800,500,100,50,red,bright_red,quitgame)
+
+        pygame.display.update()
+        clock.tick(15)
+        
+def tutorialMenu():
+    # Logic for tutorial page can go in this function
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+                
+        screen.fill(grey) 
+        largeText = pygame.font.SysFont("comicsansms",90) 
+        TextSurf, TextRect = text_objects("Tutorial", largeText) 
+        TextRect.center = ((display_width/2),(display_height*0.15)) 
+        screen.blit(TextSurf, TextRect)
+
+        button("Make these images",800,200,100,50,green,bright_green,quitgame)
+        button("Menu",800,500,100,50,red,bright_red,quitgame)
 
         pygame.display.update()
         clock.tick(15)
     
-
 pygame.init()
+# =============================================================================
+# display_width = 1600
+# display_height = 800
+# screen = gameScreen.screen
+# =============================================================================
+
 display_width = 1600
 display_height = 800
-screen = gameScreen.screen
+screen = pygame.display.set_mode((display_width,display_height))
 
 black = (0,0,0)
 white = (255,255,255)
@@ -73,18 +116,15 @@ blue = (0,0,255)
 green = (0,200,0)
 bright_red = (255,0,0)
 bright_green = (0,255,0)
+grey = (128,128,128)
  
 block_color = (53,115,255)
  
 car_width = 73
  
-pygame.display.set_caption('Smash Bros')
+pygame.display.set_caption("Smash Bros")
 clock = pygame.time.Clock()
- 
-carImg = pygame.draw.rect(screen,blue,(200,150,100,50))
 
-game_intro()
-game_loop()
+mainMenu()
 pygame.quit()
-quit()    
-
+quit()
